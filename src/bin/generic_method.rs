1ec6f3f82e5b67743b6d0d373ee0aba248af76de
amalnathsathyan <amalnathsathyan@gmail.com>
2026-01-06 08:04:19 +0530

    commented out concrete examples

--- src/bin/generic_method.rs
+++ src/bin/generic_method.rs
@@ -1,13 +1,29 @@
 #![allow(unused)]
 
 #[derive(Debug)]
+
 struct Point <T> {
     x: T,
     y:T,
 }
+// struct Point {
+//     x: u32,
+//     y:u32,
+// }
 
 //we have a struct and associated method
 //let's make it generic by replacing concrete data types
+
+// impl Point {
+//     fn new(x:u32, y:u32) -> Self {
+//         Self { x, y }
+//     }
+
+//     fn move_to(&mut self, x:u32, y:u32) {
+//         self.x = x;
+//         self.y = y;
+//     }
+// }
 
 impl<T> Point<T> {
     fn new(x:T, y:T) -> Self {
